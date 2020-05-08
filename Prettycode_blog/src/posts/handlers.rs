4ea8dff7a407602781ad762ecfc744ea651027da
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-05-08 21:48:23 +0300

    h

--- Prettycode_blog/src/posts/handlers.rs
+++ Prettycode_blog/src/posts/handlers.rs
@@ -7,31 +7,10 @@ use rocket::request::{Form, FlashMessage};
 use rocket::response::{Flash, Redirect};
 
 #[get("/new_post")]
-pub fn new_post(signal: Option<FlashMessage<'_, '_>>) -> Template {
-    let mut context: HashMap<&str, &str> = HashMap::new();
-    if let Some(signal) = signal {
-        let signal_error = signal.msg();
-        context.insert("validation-error".into(), signal_error.into());
-    }
-    Template::render("new_post", context)
+pub fn new_post() -> Template {
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("new_post", &context)
 }
 
 #[post("/new_post", data="<form>")]
-pub fn new_post_form(form: Form<NewPostForm>) -> Flash<Redirect> {
-    let form = form.into_inner();
-
-    if form.name == "" {
-        Flash::error(Redirect::to("/new_post"), "no-name");}
-
-    if form.body == "" {
-        Flash::error(Redirect::to("/new_post"), "no-body");}
-
-    if form.name == "" && form.body == "" {
-        Flash::error(Redirect::to("/new_post"), "no-body-name");}
-
-    if form.name != "" && form.body != "" {
-        Flash::success(Redirect::to("/new_post"), "all working")
-    } else {
-        Flash::error(Redirect::to("/new_post"), "ololo")
-    }
-}
+pub fn new_post_form(form: Form<NewPostForm>) -> Flash<Redirect> {todo!}
